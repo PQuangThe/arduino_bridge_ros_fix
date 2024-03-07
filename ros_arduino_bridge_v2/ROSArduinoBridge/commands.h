43a6ef1e1049c993632aa73a22f4c17c99563ff7
PQuangThe <phamthehclw2412@gmail.com>
2024-03-07 11:05:58 +0700

    7-3

---
+++ ros_arduino_bridge_v2/ROSArduinoBridge/commands.h
@@ -0,0 +1,20 @@
+/* Define single-letter commands that will be sent by the PC over the
+   serial link.
+*/
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+
+#define READ_ENCODERS       'e'
+#define MOTOR_SPEEDS        'm'
+#define MOTOR_RAW_PWM       'o'
+#define RESET_ENCODERS      'r'
+#define UPDATE_PID_LEFT     'u'
+#define UPDATE_PID_RIGHT    'p'
+#define LEFT                 0
+#define RIGHT                1
+
+#endif
+
+
